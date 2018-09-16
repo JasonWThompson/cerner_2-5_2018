9293c5edee39d9b51baecf51a33613fbe78b25ef
Jason W. Thompson <Jason.Thompson@Cerner.com>
2018-09-16 11:44:02 -0500

    Add day 4 script

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Entries for the Cerner 2^5th competition for 2018
 |1  |CompetitionReminder.ps1|Powershell      |5            |
 |2  |Kill-BadWebApp.bat     |Batch           |3            |
 |3  |score.gs               |Google AppScript|18           |
+|4  |timetravel.py          |Python          |4            |
--- timetravel.py
+++ timetravel.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # timetravel.py
 # Author: Jason W. Thompson
 # Commentary: Oh shoot! I was so busy watching WWE's "Hell in a Cell" on the WWE Network that I completley forgot to do
@@ -6,11 +8,15 @@
 # (Queue some "Huey Lewis and the News" music). 
 # Nothing's more productive than time travel after all. This python script will change the date of the last commit to
 # appear that it happened yesterday.
-# I don't know Python, so I leveraged quite a bit of help from: 
+# I don't know Python, so I leveraged quite a bit of help from the following: 
 # https://stackoverflow.com/questions/15344710/get-yesterdays-date-in-python-dst-safe
+# https://stackoverflow.com/questions/89228/calling-an-external-command-in-python
+# And a shout out to "Code with Hugo" for the syntax on manipulating git checkin dates.
+# https://codewithhugo.com/change-the-date-of-a-git-commit/
 
 from datetime import datetime, timedelta
+from subprocess import call
 
-dt_naive = datetime.today() - timedelta(days=1)
+yesterday = datetime.today() - timedelta(days=1)
 
-print(dt_naive)
+call(["git", "commit", "--amend", "--no-edit", "--date", yesterday])
